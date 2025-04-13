bbcbc9b7a7e3730f75514f7174e321be0ab1d1ef
Dorian <dorian.erhan@gmail.com>
2025-04-13 19:46:48 +0200

    Fix pgpool name resolution issue in swarm

--- order/wait-for-kafka.sh
+++ order/wait-for-kafka.sh
@@ -7,5 +7,11 @@ until nc -z kafka 9092; do
 done
 echo "Kafka is ready"
 
+echo "Waiting for database..."
+until timeout 1 bash -c "cat < /dev/null > /dev/tcp/order-pgpool/5432" 2>/dev/null; do
+  echo "Database is unavailable - sleeping"
+  sleep 2
+done
+
 python init_db.py
 exec gunicorn -b 0.0.0.0:5000 -w 2 --timeout 300 --log-level=info --reload run:app
--- payment/wait-for-kafka.sh
+++ payment/wait-for-kafka.sh
@@ -4,5 +4,12 @@ until nc -z kafka 9092; do
   sleep 1
 done
 echo "Kafka is ready"
+
+echo "Waiting for database..."
+until timeout 1 bash -c "cat < /dev/null > /dev/tcp/order-pgpool/5432" 2>/dev/null; do
+  echo "Database is unavailable - sleeping"
+  sleep 2
+done
+
 python init_db.py
 exec gunicorn -b 0.0.0.0:5000 -w 2 --timeout 30 --log-level=info --reload app:app
